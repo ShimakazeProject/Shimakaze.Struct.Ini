1af5e63006450a3a77caf0205c05b8e3fccf0988
舰队的偶像-岛风酱！ <frg2089@foxmail.com>
2020-08-16 13:02:43 +0800

    debug

---
+++ docs/js/docsify.js
@@ -0,0 +1,26 @@
+window.$docsify = {
+    loadNavbar: true,
+    loadSidebar: true,
+    coverpage: false,
+    auto2top: true,
+    maxLevel: 3,
+    name: 'Shimakaze Docs',
+    repo: 'frg2089/Shimakaze.Struct.Ini',
+    plugins: [
+        function (hook, vm) {
+            hook.beforeEach(function (html) {
+                return html
+                    + '\n\n----  \n'
+                    + '`Shimakaze Docs` Powered by [`docsify`](//docsify.js.org)  \n'
+                    + '`JavaScript` Build by [`TypeScript`](//www.typescriptlang.org)  \n'
+                    + '----';
+            }),
+                hook.afterEach(function (html) {
+                    if (vm.route.path === '/') {
+                        return html;
+                    }
+                    return html;
+                });
+        }
+    ]
+};
--- docs/ts/build.ps1
+++ docs/ts/build.ps1
@@ -4,7 +4,7 @@ Set-Location "ts";
 $fs = Get-ChildItem; # 获取当前目录内容
 foreach ($f in $fs) {
   if ($f.Extension.ToLower().Equals(".ts")) {
-    tsc -w --outFile "../js/$($f.Name.SubString(0,$f.Name.Length-$f.Extension.Length)).js" $f.Name
+    tsc --outFile "../js/$($f.Name.SubString(0,$f.Name.Length-$f.Extension.Length)).js" $f.Name
   }
 }
 Write-Output "tsc is done!"
